a6944f532808dce78296f83b47e3d5a3a5594a0e
Mark Elbre <mark@GEORG-PC.pebblelearning.lan>
2021-08-31 13:23:10 +0100

    Correctional commit

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -1,18 +1,10 @@
 //Typical hello world to signify the script running.
 console.log("Hello world.");
 
-//Function.
-function something() {
-
-    //Changing text in an element.
-    var message = "Hello again (this is from a script)";
-    var element = document.getElementById("target");
-    if (element) {
-        element.innerText = message;
-    } else {
-        console.log("Didn't find the element.") 
-    }
-
+//--Functions
+function initialise() {
+    
 }
-//Event listener.
-document.addEventListener("DOMContentLoaded", something);
+
+//--Event listeners.
+document.addEventListener("DOMContentLoaded", initialise);
