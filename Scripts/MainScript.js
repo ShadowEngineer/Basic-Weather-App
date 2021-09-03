75e514a612d5928d4ff0780b5564c740b529b64d
Mark Elbre <mark@GEORG-PC.pebblelearning.lan>
2021-09-03 10:54:26 +0100

    Hopefully a final commit for the project. Added a shed load of CSS and made the site follow a uniform colour palette. Some small transforms and transitions were added too. Pretty proud if I do say so myself.

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -1,7 +1,3 @@
-//Typical hello world to signify the script running.
-console.log("Hello world.");
-
-//--Variables.
 var url = "https://api.openweathermap.org/data/2.5/weather?";
 var url_onecall = "https://api.openweathermap.org/data/2.5/onecall?";
 var key = "aed65e96d44cd6a9cd2c8303a695c3b1";
@@ -19,11 +15,15 @@ var months = {
     10 : "October",
     11 : "November",
     12 : "December"
-};
+}
+var title_info = {
+    title_1_city : "",
+    title_1_countrycode : "",
+    title_2_city : "",
+    title_2_countrycode : "",
+}
 
-//--Functions
 function convert_wind_direction(dir) {
-    //Converting the bearing (deg) into one of the 8 main directions.
     let offset = 22.5;
     if (dir <= 0 + offset && dir > 0 - offset) {
         return "north";
@@ -45,14 +45,12 @@ function convert_wind_direction(dir) {
 }
 
 function display_info(data, target_element_id, forecast) {
-    //Locating the target element to insert the text into.
     let element = document.getElementById(target_element_id)
     if (!element) {console.log(`Invalid target_element_id: ${target_element_id}`);}
-
-    //Handling the data differently based on if it's in standard or forecast format.
+    element.parentElement.parentElement.style.display = "flex";
+    
     if (!forecast) {
         if (data.cod != 404) {
-            //Constructing the string of information.
             let date = new Date();
             date.setTime(data.dt * 1000);
             let str = "";
@@ -65,18 +63,15 @@ function display_info(data, target_element_id, forecast) {
             str += `Data gathered on ${date.toLocaleDateString()} at ${date.toLocaleTimeString()}.`
             element.innerText = str;
             
-            //Formatting and displaying the icon representing the weather.
             let icon_element = document.getElementById(target_element_id + "_img");
             icon_element.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
             icon_element.style.display = "block";
             
         } else {
-            //Error handling.
             element.innerHTML = "No valid information. Retry again.";
             console.log(`No valid data, code ${data.cod}`);
         }
     } else {
-        //Constructing the string of information.
         let date = new Date();
         let current_hour = date.getHours();
         let data_object = data.hourly[current_hour];
@@ -89,7 +84,6 @@ function display_info(data, target_element_id, forecast) {
         str += `Average visibility might be at ${data_object.visibility}m.`
         element.innerText = str;
         
-        //Formatting and displaying the icon representing the weather.
         let icon_element = document.getElementById(target_element_id.substring(0, target_element_id.length-4) + "_img_new");
         icon_element.setAttribute("src", `https://openweathermap.org/img/wn/${data_object.weather[0].icon}@2x.png`);
         icon_element.style.display = "block";
@@ -97,16 +91,15 @@ function display_info(data, target_element_id, forecast) {
 }
 
 function city_request_callback(event) {
-    //Parsing the JSON data.
     const data = JSON.parse(this.responseText);
 
-    //Displaying the data in the first div, and creating a new div to show the forecasted data.
     if (data.name) {
         console.log(data);
-        document.getElementById("target_title").innerText = `Weather at ${data.name}, ${data.sys.country}`;
+        title_info.title_1_city = data.name;
+        title_info.title_1_countrycode = data.sys.country;
+        document.getElementById("target_title").innerText = `Weather at ${title_info.title_1_city}, ${title_info.title_1_countrycode}`;
         display_info(data, "target");
 
-        //Setting the parameters for a new XMLHttp request to get the forecasted weather.
         let request_2 = new XMLHttpRequest();
         request_2.addEventListener("load", city_request_callback)
         request_2.open("GET", `${url_onecall}lat=${data.coord.lat}&lon=${data.coord.lon}&units=${units}&appid=${key}`);
@@ -117,12 +110,11 @@ function city_request_callback(event) {
         let existing_node = document.getElementById("city_forecast_container");
         if (existing_node) {
             console.log("append to existing node");
-            document.getElementById("target_title_new").innerText = `Weather in one day at ${(data.timezone+"").split("/")[1]}, ${(data.timezone+"").split("/")[0]}`;
+            document.getElementById("target_title_new").innerText = `Weather in one day at ${title_info.title_1_city}, ${title_info.title_1_countrycode}`;
             display_info(data, "target_new", true);
         } else {
             console.log("creating new node");
 
-            //Cloning the existing weather <div> element and its descendants.
             let root_node = document.getElementById("root");
             let original_node = document.getElementById("city_container");
             let new_node = original_node.cloneNode(true);
@@ -141,26 +133,25 @@ function city_request_callback(event) {
                 }
             })
 
-            //Displaying the new info.
-            document.getElementById("target_title_new").innerText = `Weather in one day at ${(data.timezone+"").split("/")[1]}, ${(data.timezone+"").split("/")[0]}`;
+            document.getElementById("target_title_new").innerText = `Weather in one day at ${title_info.title_1_city}, ${title_info.title_1_countrycode}`;
             display_info(data, "target_new", true);
         }
     }
 }
 
 function coord_request_callback(event) {
-    //Parsing the JSON data.
     const data = JSON.parse(this.responseText);
 
-    //Displaying the data in the first div, and creating a new div to show the forecasted data.
     if (data.name) {
         console.log(data);
         document.getElementById("target_2_title").innerText = `Weather at ${data.name}, ${data.sys.country}`;
+        title_info.title_2_city = data.name;
+        title_info.title_2_countrycode = data.sys.country;
         display_info(data, "target_2");
     } else if (data.timezone) {
         console.log(data);
-
-        //Cloning the existing weather <div> element and its descendants.
+        
+        //I'd rather clone an existing div rather than have on already on the page. Saves time and HTML work.
         let root_node = document.getElementById("root");
         let original_node = document.getElementById("coord_container");
         let new_node = original_node.cloneNode(true);
@@ -180,35 +171,27 @@ function coord_request_callback(event) {
             }
         })
 
-        document.getElementById("target_2_title_new").innerText = `Weather in one day at ${(data.timezone+"").split("/")[1]}, ${(data.timezone+"").split("/")[0]}`;
         display_info(data, "target_2_new", true);
+        document.getElementById("target_2_title_new").innerText = `Weather in one day at ${title_info.title_2_city}, ${title_info.title_2_countrycode}`;
     }
 }
 
 function query_location_city(city_name) {
-    //Setting parameters for the XMLHttp request.
     let request = new XMLHttpRequest();
-    
-    //Sending the request and handling the callback.
     request.addEventListener("load", city_request_callback);
     request.open("GET", `${url}q=${city_name}&units=${units}&appid=${key}`);
     request.send();
 }
 
 function query_location_coord(data) {
-    //Getting coordinates.
     const lat = data.coords.latitude
     const long = data.coords.longitude
 
-    //Setting parameters for the XMLHttp request.
     let request = new XMLHttpRequest();
-    
-    //Sending the request and handling the callback.
     request.addEventListener("load", coord_request_callback);
     request.open("GET", `${url}lat=${lat}&lon=${long}&units=${units}&appid=${key}`);
     request.send();
 
-    //Setting parameters for the new XMLHttp request to get the forecast information too.
     let request_2 = new XMLHttpRequest();
     request_2.addEventListener("load", coord_request_callback)
     request_2.open("GET", `${url_onecall}lat=${lat}&lon=${long}&units=${units}&appid=${key}`);
@@ -216,10 +199,8 @@ function query_location_coord(data) {
 }
 
 function submissionHandler(event) {
-    //Preventing the page from resetting.
     event.preventDefault();
 
-    //Searching for the city name.
     let location_box = document.getElementById("location_box");
     if (location_box.value != "") {
         query_location_city(location_box.value, "target");
@@ -227,7 +208,6 @@ function submissionHandler(event) {
 }
 
 function initialise() {
-    //Processing date and time.
     let date = new Date();
     let day = date.getDay() + ""
     let sub = day.substring(day.length - 1)
@@ -241,18 +221,15 @@ function initialise() {
         day += "th";
     }
 
-    //Displaying the date and updating time every second.
     document.getElementById("date").innerText = `Today, ${months[date.getMonth()]} ${day} in the year ${date.getFullYear()}`;
     var timer = setInterval(function(){
         date = new Date();
         document.getElementById("time").innerText = `At, ${date.toLocaleTimeString()}`;
     }, 1000);
 
-    //Listening to the form being submitted with event data.
     let input_form = document.getElementById("form");
     input_form.addEventListener("submit", submissionHandler);
 
-    //Identifying the location of the user and querying it based off that.
     let GeoLocationAPI = navigator.geolocation;
     let location;
     if (GeoLocationAPI) {
@@ -262,5 +239,4 @@ function initialise() {
     }
 }
 
-//--Event listeners.
 document.addEventListener("DOMContentLoaded", initialise);
